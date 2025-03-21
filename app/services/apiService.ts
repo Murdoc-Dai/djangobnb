ce1ccb64aaaad23c5ef04c9c6aa24b0765b068e4
Valentin Meduri <medurimora@gmail.com>
2025-03-21 01:20:15 -0500

    Primer commit del proyecto

---
+++ app/services/apiService.ts
@@ -0,0 +1,88 @@
+import { getAccessToken } from "../lib/actions";
+
+const apiService = {
+    get: async function (url: string): Promise<any> {
+        console.log('get', url);
+
+        const token = await getAccessToken();
+
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+                .then(response => response.json())
+                .then((json) => {
+                    console.log('Response:', json);
+
+                    resolve(json);
+                })
+                .catch((error => {
+                    reject(error);
+                }))
+        })
+    },
+
+    post: async function(url: string, data: any): Promise<any> {
+        console.log('post', url, data);
+
+        const token = await getAccessToken();
+
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then((json) => {
+                    console.log('Response:', json);
+
+                    resolve(json);
+                })
+                .catch((error => {
+                    reject(error);
+                }))
+        })
+    },
+
+    postWithoutToken: async function(url: string, data: any): Promise<any> {
+        console.log('postWithoutToken', url, data);
+
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: 'POST',
+                body: JSON.stringify(data), // Convert data to JSON string
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        return reject(new Error(`HTTP error! status: ${response.status}`));
+                    }
+                    return response.json();
+                })
+                .then((json) => {
+                    console.log('Response:', json);
+
+                    resolve(json);
+                })
+                .catch((error) => {
+                    console.error('Fetch error:', error);
+                    reject(error);
+                });
+        })
+    }
+}
+
+export default apiService;
